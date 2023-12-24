aa968c7daef1e9c62055f12f893b3a55c33ce672
Kanji Tanaka <sumireminami@gmail.com>
2023-12-24 10:16:03 +0900

    Remove unused options

--- src/operations/put_item.rs
+++ src/operations/put_item.rs
@@ -2,10 +2,7 @@ use super::{DynamodbTable, Error, Item};
 
 use aws_sdk_dynamodb::{
     operation::put_item::{builders::PutItemInputBuilder, PutItemInput, PutItemOutput},
-    types::{
-        AttributeValue, ConditionalOperator, ExpectedAttributeValue, ReturnConsumedCapacity,
-        ReturnItemCollectionMetrics, ReturnValue, ReturnValuesOnConditionCheckFailure,
-    },
+    types::{AttributeValue, ReturnValue},
     Client,
 };
 use std::collections::HashMap;
@@ -15,17 +12,10 @@ pub trait PutItem<'a>: DynamodbTable<'a> + Into<Item> {
     fn put_item() -> PutItemOperation<Self> {
         let input_builder = PutItemInput::builder()
             .table_name(Self::TABLE_NAME)
-            .set_expected(Self::expected())
             .set_return_values(Self::return_values())
-            .set_return_consumed_capacity(Self::return_consumed_capacity())
-            .set_return_item_collection_metrics(Self::return_item_collection_metrics())
-            .set_conditional_operator(Self::conditional_operator())
             .set_condition_expression(Self::condition_expression())
             .set_expression_attribute_names(Self::expression_attribute_names())
-            .set_expression_attribute_values(Self::expression_attribute_values())
-            .set_return_values_on_condition_check_failure(
-                Self::return_values_on_condition_check_failure(),
-            );
+            .set_expression_attribute_values(Self::expression_attribute_values());
 
         PutItemOperation {
             item: None,
@@ -33,43 +23,14 @@ pub trait PutItem<'a>: DynamodbTable<'a> + Into<Item> {
         }
     }
 
-    /// Return values to be passed as `Expected` to [`PutItemInput`].
-    /// Default is None.
-    ///
-    /// You should overwrite this method only if you use `Expected` option.
-    fn expected() -> Option<HashMap<String, ExpectedAttributeValue>> {
-        None
-    }
-
     /// Return values to be passed as `ReturnValues` to [`PutItemInput`].
     /// Default is None.
     ///
     /// You should overwrite this method only if you use `ReturnValues` option.
+    /// Only `None` or `ALL_OLD` are valid. If you set `ALL_OLD` as this value,
+    /// the [`PutItemOperation::send`] returns an old object as Option<T> otherwise
+    /// it returns None as Option<T>.
     fn return_values() -> Option<ReturnValue> {
-        None
-    }
-
-    /// Return values to be passed as `ReturnConsumedCapacity` to [`PutItemInput`].
-    /// Default is None.
-    ///
-    /// You should overwrite this method only if you use `ReturnConsumedCapacity` option.
-    fn return_consumed_capacity() -> Option<ReturnConsumedCapacity> {
-        None
-    }
-
-    /// Return values to be passed as `ReturnItemCollectionMetrics` to [`PutItemInput`].
-    /// Default is None.
-    ///
-    /// You should overwrite this method only if you use `ReturnItemCollectionMetrics` option.
-    fn return_item_collection_metrics() -> Option<ReturnItemCollectionMetrics> {
-        None
-    }
-
-    /// Return values to be passed as `ConditionalOperator` to [`PutItemInput`].
-    /// Default is None.
-    ///
-    /// You should overwrite this method only if you use `ConditionalOperator` option.
-    fn conditional_operator() -> Option<ConditionalOperator> {
         None
     }
 
@@ -94,14 +55,6 @@ pub trait PutItem<'a>: DynamodbTable<'a> + Into<Item> {
     ///
     /// You should overwrite this method only if you use `ExpressionAttributeValues` option.
     fn expression_attribute_values() -> Option<HashMap<String, AttributeValue>> {
-        None
-    }
-
-    /// Return values to be passed as `ReturnValuesOnConditionCheckFailure` to [`PutItemInput`].
-    /// Default is None.
-    ///
-    /// You should overwrite this method only if you use `ReturnValuesOnConditionCheckFailure` option.
-    fn return_values_on_condition_check_failure() -> Option<ReturnValuesOnConditionCheckFailure> {
         None
     }
 }
